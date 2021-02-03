2f76e410fd96b51bbb307980ede0a944734e7903
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2021-02-03 21:34:52 +0000

    fixed background attachment check
    Had to use CheckForIllegalCrossThreadCalls = false; , i dont know if this will cause problems.

--- Forza-Mods-AIO/MainWindow.cs
+++ Forza-Mods-AIO/MainWindow.cs
@@ -63,6 +63,7 @@ namespace Forza_Mods_AIO
             keyboardHook.Install();
             keyboardHook.KeyDown += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyDown);
             keyboardHook.KeyUp += new KeyboardHook.KeyboardHookCallback(keyboardHook_KeyUp);
+            CheckForIllegalCrossThreadCalls = false;
         }
         private void MainWindow_Load(object sender, EventArgs e)
         {
@@ -622,9 +623,16 @@ namespace Forza_Mods_AIO
         }
         private void CheckAttachedworker_DoWork(object sender, DoWorkEventArgs e)
         {
-            if (IsAttached == false && Tab_1Info.Visible == false)
+            while (true)
             {
-                Tab_1Info.Show();
+                if (IsAttached == false && Tab_1Info.Visible == false)
+                {
+                   ClearColours();
+                   BTN_TabInfo.BackColor = Color.FromArgb(45, 45, 48);
+                   Panel_Info.BackColor = Color.FromArgb(150, 11, 166);
+                   ClearTabItems();
+                   Tab_1Info.Show();
+                }
             }
         }
         private void InitialBGworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
