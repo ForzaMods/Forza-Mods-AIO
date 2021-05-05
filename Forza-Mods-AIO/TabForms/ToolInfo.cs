4f92b7215037f27da4471ef553702c7bfb35abd7
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2021-05-05 12:17:43 +0100

    Start

--- Forza-Mods-AIO/TabForms/ToolInfo.cs
+++ Forza-Mods-AIO/TabForms/ToolInfo.cs
@@ -17,15 +17,15 @@ namespace Forza_Mods_AIO
 
         private void DraffsYTLink_Click(object sender, System.EventArgs e)
         {
-            Process.Start("explorer.exe", "https://www.youtube.com/channel/UCwQ8XprkEbBJ3UaBYT_F8jA");
+            System.Diagnostics.Process.Start("explorer.exe", "https://www.youtube.com/channel/UCwQ8XprkEbBJ3UaBYT_F8jA");
         }
         private void UCPostLink_Click(object sender, System.EventArgs e)
         {
-            Process.Start("explorer.exe", "https://www.unknowncheats.me/forum/other-games/415227-fh4-speed-hack.html");
+            System.Diagnostics.Process.Start("explorer.exe", "https://www.unknowncheats.me/forum/other-games/415227-fh4-speed-hack.html");
         }
         private void DiscordLink_Click(object sender, System.EventArgs e)
         {
-            Process.Start("explorer.exe", "https://discord.gg/PQNxeYWUy9");
+            System.Diagnostics.Process.Start("explorer.exe", "https://discord.gg/PQNxeYWUy9");
         }
     }
 
