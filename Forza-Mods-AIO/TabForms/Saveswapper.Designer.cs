5d466168b63a5b8cb8955ba925103cdf913dfa1f
ItsLogic <xboxjack1@gmail.com>
2021-05-08 00:40:01 +0100

    added xuid to gamertag resolving

--- Forza-Mods-AIO/TabForms/Saveswapper.Designer.cs
+++ Forza-Mods-AIO/TabForms/Saveswapper.Designer.cs
@@ -29,17 +29,152 @@ namespace Forza_Mods_AIO.TabForms
         /// </summary>
         private void InitializeComponent()
         {
-            this.label4 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Saveswapper));
+            this.Radio_MS = new System.Windows.Forms.RadioButton();
+            this.Radio_Steam = new System.Windows.Forms.RadioButton();
+            this.LST_Accounts = new System.Windows.Forms.ListBox();
+            this.LST_Savegames = new System.Windows.Forms.ListBox();
+            this.TB_ACAutoshow = new System.Windows.Forms.CheckBox();
+            this.TXT_ACGuide = new System.Windows.Forms.RichTextBox();
+            this.BTN_SwapSave = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.LBL_ACSortingMethod = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // label4
+            // Radio_MS
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(449, 215);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 15);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Saveswapping Tab";
+            this.Radio_MS.AutoSize = true;
+            this.Radio_MS.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Radio_MS.Location = new System.Drawing.Point(60, 68);
+            this.Radio_MS.Name = "Radio_MS";
+            this.Radio_MS.Size = new System.Drawing.Size(129, 23);
+            this.Radio_MS.TabIndex = 28;
+            this.Radio_MS.TabStop = true;
+            this.Radio_MS.Text = "Microsoft Store";
+            this.Radio_MS.UseVisualStyleBackColor = true;
+            // 
+            // Radio_Steam
+            // 
+            this.Radio_Steam.AutoSize = true;
+            this.Radio_Steam.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Radio_Steam.Location = new System.Drawing.Point(60, 93);
+            this.Radio_Steam.Name = "Radio_Steam";
+            this.Radio_Steam.Size = new System.Drawing.Size(69, 23);
+            this.Radio_Steam.TabIndex = 29;
+            this.Radio_Steam.TabStop = true;
+            this.Radio_Steam.Text = "Steam";
+            this.Radio_Steam.UseVisualStyleBackColor = true;
+            // 
+            // LST_Accounts
+            // 
+            this.LST_Accounts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.LST_Accounts.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LST_Accounts.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LST_Accounts.ForeColor = System.Drawing.Color.White;
+            this.LST_Accounts.FormattingEnabled = true;
+            this.LST_Accounts.ItemHeight = 18;
+            this.LST_Accounts.Location = new System.Drawing.Point(12, 300);
+            this.LST_Accounts.Name = "LST_Accounts";
+            this.LST_Accounts.Size = new System.Drawing.Size(236, 126);
+            this.LST_Accounts.TabIndex = 30;
+            // 
+            // LST_Savegames
+            // 
+            this.LST_Savegames.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.LST_Savegames.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LST_Savegames.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LST_Savegames.ForeColor = System.Drawing.Color.White;
+            this.LST_Savegames.FormattingEnabled = true;
+            this.LST_Savegames.ItemHeight = 18;
+            this.LST_Savegames.Items.AddRange(new object[] {
+            "save1",
+            "save2",
+            "save3",
+            "=================",
+            "customsave1",
+            "customsave2",
+            "customsave3",
+            "=================",
+            "autobackup1",
+            "autobackup2",
+            "autobackup3"});
+            this.LST_Savegames.Location = new System.Drawing.Point(254, 12);
+            this.LST_Savegames.Name = "LST_Savegames";
+            this.LST_Savegames.Size = new System.Drawing.Size(234, 414);
+            this.LST_Savegames.TabIndex = 31;
+            // 
+            // TB_ACAutoshow
+            // 
+            this.TB_ACAutoshow.AutoSize = true;
+            this.TB_ACAutoshow.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TB_ACAutoshow.Location = new System.Drawing.Point(51, 122);
+            this.TB_ACAutoshow.Name = "TB_ACAutoshow";
+            this.TB_ACAutoshow.Size = new System.Drawing.Size(147, 23);
+            this.TB_ACAutoshow.TabIndex = 33;
+            this.TB_ACAutoshow.Text = "Auto Backup Save";
+            this.TB_ACAutoshow.UseVisualStyleBackColor = true;
+            // 
+            // TXT_ACGuide
+            // 
+            this.TXT_ACGuide.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.TXT_ACGuide.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXT_ACGuide.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.TXT_ACGuide.Font = new System.Drawing.Font("Open Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXT_ACGuide.ForeColor = System.Drawing.Color.White;
+            this.TXT_ACGuide.Location = new System.Drawing.Point(491, 1);
+            this.TXT_ACGuide.Margin = new System.Windows.Forms.Padding(0);
+            this.TXT_ACGuide.Name = "TXT_ACGuide";
+            this.TXT_ACGuide.ReadOnly = true;
+            this.TXT_ACGuide.Size = new System.Drawing.Size(511, 425);
+            this.TXT_ACGuide.TabIndex = 34;
+            this.TXT_ACGuide.TabStop = false;
+            this.TXT_ACGuide.Text = resources.GetString("TXT_ACGuide.Text");
+            // 
+            // BTN_SwapSave
+            // 
+            this.BTN_SwapSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.BTN_SwapSave.FlatAppearance.BorderSize = 0;
+            this.BTN_SwapSave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.BTN_SwapSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.BTN_SwapSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTN_SwapSave.Font = new System.Drawing.Font("Open Sans", 11F);
+            this.BTN_SwapSave.ForeColor = System.Drawing.Color.White;
+            this.BTN_SwapSave.Location = new System.Drawing.Point(12, 12);
+            this.BTN_SwapSave.Name = "BTN_SwapSave";
+            this.BTN_SwapSave.Size = new System.Drawing.Size(236, 50);
+            this.BTN_SwapSave.TabIndex = 27;
+            this.BTN_SwapSave.TabStop = false;
+            this.BTN_SwapSave.Text = "Swap Saves";
+            this.BTN_SwapSave.UseVisualStyleBackColor = false;
+            this.BTN_SwapSave.Click += new System.EventHandler(this.BTN_SwapSave_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(33)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(75)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(70)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Open Sans", 11F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 50);
+            this.button1.TabIndex = 35;
+            this.button1.TabStop = false;
+            this.button1.Text = "Backup Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LBL_ACSortingMethod
+            // 
+            this.LBL_ACSortingMethod.AutoSize = true;
+            this.LBL_ACSortingMethod.Font = new System.Drawing.Font("Open Sans", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_ACSortingMethod.Location = new System.Drawing.Point(12, 278);
+            this.LBL_ACSortingMethod.Name = "LBL_ACSortingMethod";
+            this.LBL_ACSortingMethod.Size = new System.Drawing.Size(116, 19);
+            this.LBL_ACSortingMethod.TabIndex = 36;
+            this.LBL_ACSortingMethod.Text = "Choose Account";
             // 
             // Saveswapper
             // 
@@ -47,7 +182,15 @@ namespace Forza_Mods_AIO.TabForms
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
             this.ClientSize = new System.Drawing.Size(1000, 445);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LBL_ACSortingMethod);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TXT_ACGuide);
+            this.Controls.Add(this.TB_ACAutoshow);
+            this.Controls.Add(this.LST_Savegames);
+            this.Controls.Add(this.LST_Accounts);
+            this.Controls.Add(this.Radio_Steam);
+            this.Controls.Add(this.Radio_MS);
+            this.Controls.Add(this.BTN_SwapSave);
             this.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -59,7 +202,14 @@ namespace Forza_Mods_AIO.TabForms
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton Radio_MS;
+        private System.Windows.Forms.RadioButton Radio_Steam;
+        private System.Windows.Forms.ListBox LST_Accounts;
+        private System.Windows.Forms.ListBox LST_Savegames;
+        private System.Windows.Forms.CheckBox TB_ACAutoshow;
+        private System.Windows.Forms.RichTextBox TXT_ACGuide;
+        private System.Windows.Forms.Button BTN_SwapSave;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LBL_ACSortingMethod;
     }
 }
--- Forza-Mods-AIO/TabForms/Saveswapper.cs
+++ Forza-Mods-AIO/TabForms/Saveswapper.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,7 +10,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace Forza_Mods_AIO.TabForms
 {
@@ -15,6 +19,73 @@ namespace Forza_Mods_AIO.TabForms
         public Saveswapper()
         {
             InitializeComponent();
+        }
+
+
+            string Get(string uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.Headers.Add("x-xbl-contract-version", "1");
+            request.Headers.Add("Accept-Encoding", "gzip; q=1.0, deflate; q=0.5, identity; q=0.1");
+            request.Headers.Add("x-xbl-contentrestrictions", "eyJ2ZXJzaW9uIjoyLCJkYXRhIjp7Imdlb2dyYXBoaWNSZWdpb24iOiJHQiIsIm1heEFnZVJhdGluZyI6MjU1LCJwcmVmZXJyZWRBZ2VSYXRpbmciOjI1NSwicmVzdHJpY3RQcm9tb3Rpb25hbENvbnRlbnQiOmZhbHNlfX0=");
+            request.Headers.Add("Signature", "AAAAAQHXQ4EQ/V7sC+5aQKD9nIHhf3yVjocERoyV0rjBwXygyruSowBOgwWFZSmlquOqLaid2BiHMMizESVEKfN8H6QZXTq2XVlhFg==");
+            request.Headers.Add("Cache-Control", "no-store, must-revalidate, no-cache");
+            request.Headers.Add("x-xbl-client-name", "XboxApp");
+            request.Headers.Add("x-xbl-market", "GB");
+            request.Headers.Add("X-XblCorrelationId", "c6c515ec-dcae-47da-b16a-01306ac67028");
+            request.Headers.Add("Accept-Language", "en-GB, en");
+            request.Headers.Add("Authorization", "XBL3.0 x=12686054406395589293;eyJlbmMiOiJBMTI4Q0JDK0hTMjU2IiwiYWxnIjoiUlNBLU9BRVAiLCJjdHkiOiJKV1QiLCJ6aXAiOiJERUYiLCJ4NXQiOiIxZlVBejExYmtpWklFaE5KSVZnSDFTdTVzX2cifQ.ewrISRuItJduj2z6RGdZ8SPpmv21qmQDCPWy-GVIrysDIalAt5fuEqntmhDeB4bXc0R1CpOAg9w4GAtGuWUs11V3g-izEmUN0cg3DpR7ogJ9nSZ_bhxSMDij1kFxUON5AbTkNg7P6ui4fIr38HClv2CmZnbKX_xR5rEUbrdLiCQ.wxo76Mi_TAZ10zdvv-uHvQ.WQi2wRfJyS-9HE3HEVoYvHVY1M9sFu6CsISEe074jTz2XanLFWvBJXQNGoipvfscS8D0Y-UR_UGXLs1o5BFj93KJ51ViYyzkf9cVPIaYkeHEDkvRtsZJac8dYJ4f0Rj7XmX2BljEb5FTgK4Zp3aUdLmkqJ_HVm1nF-iT6xt9xuARBhF6Q4N148jK03JMbpwcK5WDIhHml6EXZWEm276a07enS28A1HJJFmaDLHuECOz6uig4fuWf4CkD7QuorlQkov0x88WizwSaUnls3yLXU1X4xpmUoPPX1bE7AkoyTx3i1kZ6pG5_GRd_FaIc3vGRB5TpxJ1fznSKnFDkQ6JdJ8S2P3VC3sZYgte4n3IOUIUd4V5xhr-eJnCRkrqB-PR3_FqoR-6-ZtYNvkG3YmD-xQfhGKliRW6kI6fWASxaHAWQOBE4rQjD8YZjAAZaZkio-r4MYyXVG-lracyAgCgkk8JKv2S2hZh0Hut0zgRGHF2-uCiXF2ERtbbpAG8jpbhVnTlgpa0zxm2eySIMCBMXaB3ewH78suA4M5ihw-Tsc6vfEJTj638rFlCdsDcm9tODz51XK0_nbRUPqeZ_QcoTmUlb6JwBygXN9lqV2zeVeAOKhd7qEKoiEVo9w7qMUu_ALHsxlyAGvh3JfL3qAw4r36ooksx9IKGV5LsHISibNZ3QPDDGxIk_S64KLu3J2dE7vbB60qsBF_ARczntgxkJXPoMRnUCTyFUNKD9eUXEfQokSx2TLPLAqpDn3U0826qUIrhziuwDzbbFroVZq6Owk79oPTqJueASy3tgGwps0YU3KtO7KOLlPhbJIYT-aPAaz9t0SQMqwBiCY6U5Ov3Fsdx1yruq7chJhSmgLw_pwSJxX0oh3V9lJ3fkLE0bCNf1myNejLh0BcjlcUMFFshKC8JIN33nu9MeCHUqcjeT3W2zJBOsQUegoTc4lIBuZ0hF2dJ9_h7llepjauS-od7_l8l-6CHOqy22BgcVtmoAG08-FF0HlJoBMI1bjNAkjeIuHi9Bp7qC0d9iWY-dnR7VdE9p_Xj9WrqUoArC07Idy5S_jgm_6qD3ba8y4bE8NeA-PWZbfr7qPPmErWQCkHqKqagg9tgvjPfShqZ_WnHL_-5_5PGHxzvdpAndIFRjdoFUpRStYP6JwFphULby57X_Ey7GUfliDtQtq14GQ0yw721eIqPyxPeye_4hmGEFVKB0rPwrmrMH_ZFiX56WMhkFaTak05NkD-TTuQNlCyUitTcIdSE-3VuBqPhGEg6pPY_ENyDjxuBS1a6M0oJb3xYph0yP6ErvLiIMMue0fpBhipD7NCtR6KB1kbIP871nZj0CrK-LSYXlU-5FiJLmibKG1z0ZMjqFnLmemRQP5ZzAEVt_sLwzOFp0FdFU3GAUkDW4P6zV2dv1JCzElZPVpS-9bnIK1dDgWTFCiqUE3DYfB7Z6CrQ6OBc9fJhmIjBImiB8r1IWPBVrcDgY7fB8WfTd_trRgTy16uvRp8bsqi_-yQm01Vasat022nyLinKKcG8zHCcYjFnAXZKFXjVv6HCkvGe85hiHZHQ1WLOIe0IhANMyDrsJ7wd3STd_P1Ndo7VXWCInZaKS1HagtzYr8TI1HOVZMNhvxhGkrKbEDBT6dCtYYSEgDqr-0sUVb9iNYhrtsNBXSj3ou7i6ix_wz0Pv0kCX1qZa4d54aT_LrOXDOrZ-KOaEojbmORW5vaL4rMl10MDGOXgPyVNLO3raZgfG6stWPXDZLfCUC2JP8-Ef1kBkELTn5wuEs7P9kCqDXkkBdJLr3j2ICXBfMab0ZtQmZ5m814ohWJCJ3ymniKAOOlFE1n985Trrhq7v46irkQ9l3kH7bH_4vOZhdPHM2WHdoiPp4VrczHPK2poEmZwTIgLUZSxDMKPxmDCEUAneccec0KDOLfpMuLHFv2eJ6EdkYew-9_ya0f7kjxREiLi5ZHM.Z8A00fvFzm0hBwkiefy4dCGvJcKTbYwhkyOJvFp3aGc");
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+
+                return reader.ReadToEnd();
+            }
+        }
+        void FindMSSave()
+        {
+            //MessageBox.Show("MS store save");
+            string path = @"C:\Users\"+ Environment.UserName + @"\AppData\Local\Packages\Microsoft.SunriseBaseGame_8wekyb3d8bbwe\SystemAppData\wgs";
+            //System.Diagnostics.Process.Start(path);
+            var dirinfo = new DirectoryInfo(path);
+            var acclist = dirinfo.EnumerateDirectories("*");
+            //
+            //MessageBox.Show( ;
+            foreach (var dir in acclist)
+            {
+                if (dir.Name != "t")
+                {
+                    var result = (dynamic)JObject.Parse(Get("https://peoplehub.xboxlive.com/users/me/people/xuids("+ Int64.Parse(dir.Name.Substring(0, 16), System.Globalization.NumberStyles.HexNumber) +")"));
+                    LST_Accounts.Items.Add(result.people[0].gamertag.ToString());
+                }
+                
+                    //LST_Accounts.Items.Add("last played: " + dir.LastWriteTime);
+
+            }
+                
+        }
+        void FindSteamSave()
+        {
+            MessageBox.Show("Steam Save");
+        }
+        private void BTN_SwapSave_Click(object sender, EventArgs e)
+        {
+            if (Radio_MS.Checked)
+                FindMSSave();
+            else if (Radio_Steam.Checked)
+                FindSteamSave();
+            else
+                MessageBox.Show("Error", "Platform not selected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+
         }
     }
 }
