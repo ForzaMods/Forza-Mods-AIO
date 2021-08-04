c59876d09215b34acd67b0e7a241aefa3bac129b
Ethan <ethanjpickering@hotmail.co.uk>
2021-08-04 23:30:34 +0100

    Finished updater

--- Forza-Mods-AIO/Properties/Resources.Designer.cs
+++ Forza-Mods-AIO/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Forza_Mods_AIO.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Updater {
+            get {
+                object obj = ResourceManager.GetObject("Updater", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
