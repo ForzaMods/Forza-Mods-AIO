c93bec145a58b28507d0b4a8c2891e3938c81f7a
comamnds <comamnds@outlook.com>
2021-12-04 16:46:23 +0000

    fixed the decals thing not working on ms store Added marks bypass for traffic/hs in garage updated addresses for Autoshow/Garage and steam stats editor

--- Forza-Mods-AIO/TabForms/StatsEditor.cs
+++ Forza-Mods-AIO/TabForms/StatsEditor.cs
@@ -94,8 +94,9 @@ namespace Forza_Mods_AIO.TabForms
                     if (Process.GetProcessesByName("ForzaHorizon5")[0].MainModule.FileName.Contains("Microsoft.624F8B84B80"))
                         yeet = (await MainWindow.m.AoBScan(ScanStartAddr, ScanEndAddr, "28 1F ? ? ? 7F 00 00", true, true)).ToList();
                     else
-                        yeet = (await MainWindow.m.AoBScan(ScanStartAddr, ScanEndAddr, "48 93 ? ? ? 7F 00 00", true, true)).ToList();
+                        yeet = (await MainWindow.m.AoBScan(ScanStartAddr, ScanEndAddr, "58 49 ? ? ? 7F 00 00", true, true)).ToList();
                 }
+                
                 foreach (var item in yeet)
                 {
                     if (MainWindow.m.ReadString((item - 76).ToString("X"), zeroTerminated: true).Length > 1
