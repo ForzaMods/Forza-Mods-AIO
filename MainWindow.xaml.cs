a589848b6dcc48b1122bd4f67e151b657f682605
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2022-04-18 02:19:21 +0100

    Clean tab switching

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace WPF_Mockup
 {
@@ -20,7 +23,8 @@ namespace WPF_Mockup
     public partial class MainWindow : Window
     {
         #region Variables
-        public static MainWindow mw;
+        public MainWindow mw = new MainWindow();
+        List<Page> windows = new List<Page>() { new Tabs.AutoShow() };
         #endregion
         #region Starting
         public MainWindow()
@@ -43,10 +47,21 @@ namespace WPF_Mockup
         }
         private void CategoryButton_Click(object sender, RoutedEventArgs e)
         {
-            foreach (System.Windows.Controls.RadioButton rb in ButtonStack.Children)
+            foreach (RadioButton rb in ButtonStack.Children)
             {
                 if ((bool)rb.IsChecked)
+                {
                     rb.Background = CustomTheming.Monet.DarkerColour;
+                    foreach(Page w in windows)
+                        if(w.Title == rb.Name)
+                        {
+                            foreach(FrameworkElement Element in Window.GetChildren(true))
+                            {
+                                if(Element.Name == rb.Name + "Frame")
+                                    Element.Visibility = Visibility.Visible;
+                            }
+                        }
+                }
                 else
                     rb.Background = CustomTheming.Monet.MainColour;
             }
@@ -56,5 +71,35 @@ namespace WPF_Mockup
             CustomTheming.Monet.ApplyMonet();
         }
         #endregion
+    }
+
+    public static class GetChildrenExtension
+    {
+        //Credit to BrainSlugs83 for the GetChildren Method (https://stackoverflow.com/questions/874380/wpf-how-do-i-loop-through-the-all-controls-in-a-window) 
+        public static IEnumerable<Visual> GetChildren(this Visual parent, bool recurse = true)
+        {
+            if (parent != null)
+            {
+                int count = VisualTreeHelper.GetChildrenCount(parent);
+                for (int i = 0; i < count; i++)
+                {
+                    // Retrieve child visual at specified index value.
+                    var child = VisualTreeHelper.GetChild(parent, i) as Visual;
+
+                    if (child != null)
+                    {
+                        yield return child;
+
+                        if (recurse)
+                        {
+                            foreach (var grandChild in child.GetChildren(true))
+                            {
+                                yield return grandChild;
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
