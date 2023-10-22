b39d50053556737fd048a975438af84baf9fb513
szaamerik <szaamerik@outlook.com>
2023-10-23 00:06:46 +0200

    Fixed anti anti cheat for ms (again)

--- Forza-Mods-AIO/Resources/Bypass.cs
+++ Forza-Mods-AIO/Resources/Bypass.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using static Memory.Imps;
 
 namespace Forza_Mods_AIO.Resources;
@@ -57,11 +58,11 @@ public abstract partial class Bypass
      * ForzaHorizon5.exe!tm_sdk_installer_unregister_changes+0xb2ef4  <- same as on thread +0x2ce8c. (multiple threads)
      * ForzaHorizon5.exe!tm_sdk_installer_unregister_changes+0xc3a58  <- same as on thread +0x2ce8c, though dies after some time.
      * ForzaHorizon5.exe!tm_sdk_installer_unregister_changes+0xeef34  <- watchdog. runs on startup (+0xef0d4 on ms)
-     * ForzaHorizon5.exe!tm_sdk_installer_unregister_changes+0x1e4b00 <- dies when you kill watchdog so not sure, might aswell suspend it too.
+     * ForzaHorizon5.exe!tm_sdk_installer_unregister_changes+0x1e4b00 <- dies when you kill watchdog so not sure, might aswell suspend it too. (+0x1e4ca0 on ms)
      * ForzaHorizon5.exe!Turn10GainGetDSPDescription+0x477710         <- I suspended it once and my breakpoints on mem integrity check functions stopped for some time
      *                                                                   (no other threads died), probably some placeholder thread that Idk what impact has on the game.
      *                                                                   I doubt its audio thread as theadmiester said, bc it has no impact on audio whatsoever.
-     * ForzaHorizon5.exe+0x9390e0                                     <- Main game threads
+     * ForzaHorizon5.exe+0x9390e0                                     <- Main game threads (+0x8e22a0 on ms)
      * ForzaHorizon5.exe!tm_api_update+0x2840                         <- Unknown thread. not sure what it does
      * ForzaHorizon5.exe+0x10d9be0                                    <- steam_api64.SteamInternal_ContextInit thread. runs only at "press start" screen
      */
@@ -73,31 +74,35 @@ public abstract partial class Bypass
         var RtlUserThreadStart = GetProcAddress(NtDLL, "RtlUserThreadStart");
         var NtCreateThreadEx = GetProcAddress(NtDLL, "NtCreateThreadEx");
         var tm_sdk_installer_unregister_changes = (nint)MainWindow.mw.m.ScanForSig("48 83 EC ? 48 8B ? ? ? ? ? FF 15 ? ? ? ? 48 8B ? ? ? ? ? BA").FirstOrDefault();
-            
+        
         // this one fixes dll injection
         MainWindow.mw.m.WriteArrayMemory(RtlUserThreadStart, new byte[] { 0x48, 0x83, 0xEC, 0x78, 0x4C, 0x8B, 0xC2 });
         // this one allows to kill threads
         MainWindow.mw.m.WriteArrayMemory(NtCreateThreadEx, new byte[] { 0x4C, 0x8B, 0xD1, 0xB8, 0xC7, 0x00, 0x00, 0x00 });
+        
+        Thread.Sleep(100);
         
         foreach (ProcessThread Thread in MainWindow.mw.gvp.Process.Threads)
         {
 
             var ThreadStartAddress = GetThreadStartAddress(Thread.Id);
             
-            if (ThreadStartAddress == (tm_sdk_installer_unregister_changes + 0xeef34) || ThreadStartAddress == (tm_sdk_installer_unregister_changes + 0xef0d4))
+            if (ThreadStartAddress == tm_sdk_installer_unregister_changes + 0xeef34 || ThreadStartAddress == tm_sdk_installer_unregister_changes + 0xef0d4)
             {
                 TerminateThread(OpenThread(ThreadAccess.Terminate, false, (uint)Thread.Id));
             }
-            else if (ThreadStartAddress == (tm_sdk_installer_unregister_changes + 0xd5f20) || ThreadStartAddress == (tm_sdk_installer_unregister_changes + 0xd60c0))
+            else if (ThreadStartAddress == tm_sdk_installer_unregister_changes + 0xd5f20 || ThreadStartAddress == tm_sdk_installer_unregister_changes + 0xd60c0)
             {
                 TerminateThread(OpenThread(ThreadAccess.Terminate, false, (uint)Thread.Id));
             }
-            else if (ThreadStartAddress == (tm_sdk_installer_unregister_changes + 0x1e4b00))
+            else if (ThreadStartAddress == tm_sdk_installer_unregister_changes + 0x1e4b00 || ThreadStartAddress == tm_sdk_installer_unregister_changes + 0x1e4ca0)
             {
                 TerminateThread(OpenThread(ThreadAccess.Terminate, false, (uint)Thread.Id));
             }
-    
+            
         }
+        
+        
     }
 
     #endregion
