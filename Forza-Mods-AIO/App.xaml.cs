1b5fd5da8e27ba97361c1a6fbad57b2cabfe6001
merika <126014478+szaaamerik@users.noreply.github.com>
2024-02-25 05:14:43 -0800

    Delete old project

--- Forza-Mods-AIO/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Forza_Mods_AIO;
-
-/// <summary>
-///     Interaction logic for App.xaml
-/// </summary>
-public partial class App
-{
-}
--- Forza-Mods-AIO/Overlay/Options/MenuOption.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Forza_Mods_AIO.Overlay.Options;
-
-public class MenuOption
-{
-    public string Name { get; protected init; } = string.Empty;
-    public string? Description { get; protected init; }
-    public bool IsEnabled { get; set; }
-}
