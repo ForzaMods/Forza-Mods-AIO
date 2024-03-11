363e5d71cd03656ab9d3534341c376734663ec83
szaaamerik <126014478+szaaamerik@users.noreply.github.com>
2024-03-11 10:49:16 -0700

    feat(ui): add a StatusComboboxItem ui element

---
+++ Forza-Mods-AIO/Forza-Mods-AIO/Controls/StatusComboboxItem/StatusComboboxItem.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace Forza_Mods_AIO.Controls.StatusComboboxItem;
+
+public class StatusComboboxItem : ComboBoxItem
+{
+    public static readonly DependencyProperty IsOnProperty
+        = DependencyProperty.Register(nameof(IsOn),
+            typeof(bool),
+            typeof(StatusComboboxItem),
+            new PropertyMetadata(default(bool)));
+    
+    [Bindable(true)]
+    [Category("Forza-Mods-AIO")]
+    public bool IsOn
+    {
+        get => (bool)GetValue(IsOnProperty);
+        set => SetValue(IsOnProperty, value);
+    }
+
+    static StatusComboboxItem()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(StatusComboboxItem), new FrameworkPropertyMetadata(typeof(StatusComboboxItem)));
+    }
+}
