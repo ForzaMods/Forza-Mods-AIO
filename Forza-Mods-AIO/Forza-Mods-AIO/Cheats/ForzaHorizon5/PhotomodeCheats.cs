0e78360a1a824be4b7ab310dd2571765111286c3
szaaamerik <126014478+szaaamerik@users.noreply.github.com>
2024-03-18 14:47:05 -0700

    feat(fh5): photo mode noclip

---
+++ Forza-Mods-AIO/Forza-Mods-AIO/Cheats/ForzaHorizon5/PhotomodeCheats.cs
@@ -0,0 +1,60 @@
+﻿using static Forza_Mods_AIO.Resources.Cheats;
+using static Forza_Mods_AIO.Resources.Memory;
+
+namespace Forza_Mods_AIO.Cheats.ForzaHorizon5;
+
+public class PhotomodeCheats : CheatsUtilities, ICheatsBase
+{
+    private UIntPtr _noClipAddress;
+    public UIntPtr NoClipDetourAddress;
+
+    public async Task CheatNoClip()
+    {
+        _noClipAddress = 0;
+        NoClipDetourAddress = 0;
+
+        const string sig = "48 89 ? ? 48 C7 45 B0 ? ? ? ? 48 8B ? ? ? ? ? 48 85";
+        _noClipAddress = await SmartAobScan(sig);
+
+        if (_noClipAddress > 0)
+        {
+            _noClipAddress -= 93;
+            if (GetClass<Bypass>().CrcFuncDetourAddress == 0)
+            {
+                await GetClass<Bypass>().DisableCrcChecks();
+            }
+            
+            if (GetClass<Bypass>().CrcFuncDetourAddress == 0) return;
+
+            var asm = new byte[]
+            {
+                0x80, 0x3D, 0x12, 0x00, 0x00, 0x00, 0x01, 0x75, 0x06, 0x31, 0xC0, 0x89, 0x44, 0x24, 0x50, 0x0F, 0x11,
+                0x44, 0x24, 0x54
+            };
+            NoClipDetourAddress = GetInstance().CreateDetour(_noClipAddress, asm, 5);
+            return;
+        }
+        
+        ShowError("Photo mode noclip", sig);
+    }
+    
+    public void Cleanup()
+    {
+        var mem = GetInstance();
+
+        if (_noClipAddress > 0)
+        {
+            mem.WriteArrayMemory(_noClipAddress, new byte[] { 0x0F, 0x11, 0x44, 0x24, 0x54 });
+            Free(NoClipDetourAddress);
+        }
+    }
+
+    public void Reset()
+    {
+        var fields = typeof(PhotomodeCheats).GetFields().Where(f => f.FieldType == typeof(UIntPtr));
+        foreach (var field in fields)
+        {
+            field.SetValue(this, UIntPtr.Zero);
+        }
+    }
+}
--- Forza-Mods-AIO/Forza-Mods-AIO/Views/SubPages/SelfVehicle/PhotoMode.xaml.cs
+++ Forza-Mods-AIO/Forza-Mods-AIO/Views/SubPages/SelfVehicle/PhotoMode.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using Forza_Mods_AIO.Cheats.ForzaHorizon5;
+using MahApps.Metro.Controls;
+using static Forza_Mods_AIO.Resources.Cheats;
+using static Forza_Mods_AIO.Resources.Memory;
 
 namespace Forza_Mods_AIO.Views.SubPages.SelfVehicle;
 
@@ -6,6 +10,29 @@ public partial class PhotoMode
 {
     public PhotoMode()
     {
+        DataContext = this;
+        
         InitializeComponent();
+    }
+
+    private static PhotomodeCheats PhotomodeCheatsFh5 => GetClass<PhotomodeCheats>();
+
+    private async void NoClipSwitch_OnToggled(object sender, RoutedEventArgs e)
+    {
+        if (sender is not ToggleSwitch toggleSwitch)
+        {
+            return;
+        }
+
+        toggleSwitch.IsEnabled = false;
+        if (PhotomodeCheatsFh5.NoClipDetourAddress == 0)
+        {
+            await PhotomodeCheatsFh5.CheatNoClip();
+        }
+        toggleSwitch.IsEnabled = true;
+
+        
+        if (PhotomodeCheatsFh5.NoClipDetourAddress == 0) return;
+        GetInstance().WriteMemory(PhotomodeCheatsFh5.NoClipDetourAddress + 0x19, toggleSwitch.IsOn ? (byte)1 : (byte)0);
     }
 }
